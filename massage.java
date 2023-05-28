5f29e97c2d3db78905c0852eec3f972f20a404da
SampleProject28May <134693149+BalaJadhav@users.noreply.github.com>
2023-05-28 17:44:41 +0530

    Add files via upload

---
+++ massage.java
@@ -0,0 +1,15 @@
+
+public class massage {
+	public static void main (String [] args ) {
+		
+		//System.out.println ("Velocity Carporate training center");
+		
+		System.out.println ("Shree Coaching Center");
+		
+		System.out.println ("Patil Hospital");
+		
+	}
+	
+}
+
+
